9f576bcccc5fc4946f952aa0797bfbddc7050076
Tiago Pereira <tiagorp2004@gmail.com>
2023-05-25 09:37:14 +0100

    Updates

---
+++ precache-manifest.aaf8eaea0a3ceea1df9ab1fda1cfec9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a78fd2388b6ee16a696",
+    "url": "/Robofriends/static/css/main.28bbd73f.chunk.css"
+  },
+  {
+    "revision": "7a78fd2388b6ee16a696",
+    "url": "/Robofriends/static/js/main.8d09cdac.chunk.js"
+  },
+  {
+    "revision": "e232bc830250045a16a9",
+    "url": "/Robofriends/static/js/runtime~main.1ad981df.js"
+  },
+  {
+    "revision": "afc0d819d87f0411e25a",
+    "url": "/Robofriends/static/css/2.2e65d300.chunk.css"
+  },
+  {
+    "revision": "afc0d819d87f0411e25a",
+    "url": "/Robofriends/static/js/2.b19faffd.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/Robofriends/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8682fbe75fb9cf0565f84241e663c31b",
+    "url": "/Robofriends/index.html"
+  }
+];
